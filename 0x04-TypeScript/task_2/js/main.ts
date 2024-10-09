487308365eba0c778f82ae0deae933e734a8a0f4
Thomasomoke <omokethomas644@gmail.com>
2024-10-09 16:44:27 +0000

    configuration files

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,50 +1,23 @@
-interface DirectorInterface {
-	workFromHome(): string;
-	getCoffeeBreak(): string;
-	workDirectorTasks(): string;
+interface student {
+        firstName: string;
+        lastName: string;
+        age: number;
+        location: string;
 }
 
-interface TeacherInterface {
-	workFromHome(): string;
-	getCoffeeBreak(): string;
-	workTeacherTasks(): string;
+const stu1: student = {
+        firstName: 'Thomas',
+        lastName: 'Omoke',
+        age: 23,
+        location: 'Mombasa'
 }
 
-class Director implements DirectorInterface {
-	workFromHome(): string {
-		return "Working from home";
-	}
-
-	getCoffeeBreak():string {
-		return "Getting a coffee break";
-	}
-	workDirectorTasks():string {
-		return "Getting to director tasks";
-	}
-
-class Teacher implements TeacherInterface {
-	workFromHome():string {
-		return "Cannot work from home";
-	}
-	getToWork():string {
-		return "Cannot have a break";
-	}
-	workTeacherTasks():string {
-		return "Getting to work";
-	}
+const stu2: student = {
+        firstName: 'Hillan',
+        lastName: 'Boyani',
+        age: 25,
+        location: 'Nairobi'
 }
 
-function createEmployee(salary: number | string): Director | Teacher {
-	if (typeof salary === "number" && salary < 500) {
-		return new Teacher();
-	} else {
-		return new Director();
-	}
-}
-
-
-
-
-	
-
+const studentsList: student[] = [ stu1, stu2];
 
